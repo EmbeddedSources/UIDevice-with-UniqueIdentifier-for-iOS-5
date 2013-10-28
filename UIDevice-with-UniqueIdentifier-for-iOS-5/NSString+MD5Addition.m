d61f81deb70014e02e33d9a6213863664a6afb77
dodikk <dodikk88.reg@gmail.com>
2013-10-28 19:37:24 +0200

    added arm64 support

--- UIDevice-with-UniqueIdentifier-for-iOS-5/NSString+MD5Addition.m
+++ UIDevice-with-UniqueIdentifier-for-iOS-5/NSString+MD5Addition.m
@@ -11,7 +11,8 @@
 
 @implementation NSString(MD5Addition)
 
-- (NSString *) stringFromMD5{
+- (NSString *) stringFromMD5
+{
 
     if(self == nil || [self length] == 0)
         return nil;
@@ -19,10 +20,13 @@
     const char *value = [self UTF8String];
 
     unsigned char outputBuffer[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(value, strlen(value), outputBuffer);
+    size_t valueLength = strlen(value);
+    CC_LONG castedValueLength = (CC_LONG)valueLength;
+    CC_MD5(value, castedValueLength, outputBuffer);
 
     NSMutableString *outputString = [ NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2 ];
-    for(NSInteger count = 0; count < CC_MD5_DIGEST_LENGTH; count++){
+    for(NSInteger count = 0; count < CC_MD5_DIGEST_LENGTH; count++)
+    {
         [outputString appendFormat:@"%02x",outputBuffer[count]];
     }
 
